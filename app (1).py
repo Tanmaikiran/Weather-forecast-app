307a84c3e5d83d330b0b9161b3a37e360644e7a1
Tanmaikiran <tanmaibattu@gmail.com>
2025-06-23 15:02:56 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -7,7 +7,7 @@ st.title("🌤️ Weather Forecast App")
 city = st.text_input("Enter city name:")
 
 if city:
-    api_key = "d0f96f39675caec883ec836a5a88b67b"  # Temporary test key
+    api_key = "2805c5e870303f6f4c409e1f71bfe426"  # Your valid key
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
     response = requests.get(url)
