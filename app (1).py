54f8a81152d80dee9e569a83ba707240ede2514b
Tanmaikiran <tanmaibattu@gmail.com>
2025-06-23 14:55:28 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import requests
 
@@ -8,7 +7,7 @@ st.title("🌤️ Weather Forecast App")
 city = st.text_input("Enter city name:")
 
 if city:
-   api_key = "2805c5e870303f6f4c409e1f71bfe426"
+    api_key = "30f62c3f394fe9bc05c60a34ee3e2d55"  # Temporary test key
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
     response = requests.get(url)
